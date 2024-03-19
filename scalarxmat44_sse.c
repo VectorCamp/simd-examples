85a00a600e5ff8993ff8547b041d0325aafff669
gtsoul-tech <gtsoulkanakis@gmail.com>
2024-03-19 13:06:34 +0200

    random_num, nanosecond calculate

--- scalarxmat44_sse.c
+++ scalarxmat44_sse.c
@@ -1,83 +1,71 @@
 #include <immintrin.h>
+#include <math.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
-#include <sys/time.h>
-
-#define N 4   // #define N (1L << 10)
+#include <time.h>
+#define N 1000000000   // loops
 
 int
 main() {
 
-    // allocate memory for array
     int are_equal = 1;
     float lamda = 5.0;
-    int count = 0;
-    float A[N][N] __attribute__((aligned(16)));
-    float B[N][N] __attribute__((aligned(16)));
-    // fill array with numbers
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            A[i][j] = (float) count;
-            count = count + 1;
-        }
-    }
-    count = 0;
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            B[i][j] = (float) count;
-            count = count + 1;
+    float A[4][4] __attribute__((aligned(16)));
+    float B[4][4] __attribute__((aligned(16)));
+    struct timespec start, mid, end;
+    // Use the current time in microseconds to seed the random number generator
+    srand(time(NULL));
+    // fill array with random numbers
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            float random_number = (float) rand() / RAND_MAX * 4;
+            A[i][j] = random_number;
+            B[i][j] = random_number;
         }
     }
 
-    struct timeval tv1, tv2, tv3, diff1, diff2;
-    gettimeofday(&tv1, NULL);
-
+    clock_gettime(CLOCK_MONOTONIC, &start);
     // normal scalar version
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            A[i][j] = lamda * A[i][j];
+    for (int k = 0; k < N; k++) {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                A[i][j] = lamda * A[i][j];
+            }
         }
     }
-    gettimeofday(&tv2, NULL);
+    clock_gettime(CLOCK_MONOTONIC, &mid);
 
     // SSE version
-    __m128 lamda_vec = _mm_set1_ps(lamda);   // create a vector with 4 copies of lambda
 
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j += 4) {
-            __m128 B_vec = _mm_load_ps(&B[i][j]);   // load 4 floats from B
-            B_vec = _mm_mul_ps(B_vec, lamda_vec);   // multiply by lamda
-            _mm_store_ps(&B[i][j], B_vec);          // store the result back in B
+    __m128 lamda_vec = _mm_set1_ps(lamda);   // create a vector with 4 copies of lambda
+    for (int k = 0; k < N; k++) {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j += 4) {
+                __m128 B_vec = _mm_load_ps(&B[i][j]);   // load 4 floats from B
+                B_vec = _mm_mul_ps(B_vec, lamda_vec);   // multiply by lamda
+                _mm_store_ps(&B[i][j], B_vec);          // store the result back in B
+            }
         }
     }
-    gettimeofday(&tv3, NULL);
-
-    // Get time difference
-    // https://stackoverflow.com/questions/1444428/time-stamp-in-the-c-programming-language
-    diff1.tv_sec = tv2.tv_sec - tv1.tv_sec;
-    diff1.tv_usec = tv2.tv_usec + (1000000 - tv1.tv_usec);
-    diff2.tv_sec = tv3.tv_sec - tv2.tv_sec;
-    diff2.tv_usec = tv3.tv_usec + (1000000 - tv2.tv_usec);
-
-    while (diff1.tv_usec > 1000000) {
-        diff1.tv_sec++;
-        diff1.tv_usec -= 1000000;
-        printf("updated diff1 tv_sec:%ld tv_usec:%ld\n", diff1.tv_sec, diff1.tv_usec);
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    long seconds1 = mid.tv_sec - start.tv_sec;
+    long nanoseconds1 = mid.tv_nsec - start.tv_nsec;
+    if (nanoseconds1 < 0) {
+        seconds1--;
+        nanoseconds1 += 1000000000;
     }
-    while (diff2.tv_usec > 1000000) {
-        diff2.tv_sec++;
-        diff2.tv_usec -= 1000000;
-        printf("updated diff2 tv_sec:%ld tv_usec:%ld\n", diff2.tv_sec, diff2.tv_usec);
+    long seconds2 = end.tv_sec - mid.tv_sec;
+    long nanoseconds2 = end.tv_nsec - mid.tv_nsec;
+    if (nanoseconds2 < 0) {
+        seconds2--;
+        nanoseconds2 += 1000000000;
     }
-
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            {
-                if (A[i][j] != B[i][j]) {
-                    are_equal = 0;
-                    break;
-                }
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            if (fabs(A[i] - B[i]) == 0) {
+                are_equal = 0;
+                break;
             }
         }
     }
@@ -86,6 +74,6 @@ main() {
     } else {
         printf("Arrays A and B are not the same\n");
     }
-    printf("scalar: %ld sec, usec: %ld\n", diff1.tv_sec, diff1.tv_usec);
-    printf("SSE   : %ld sec, usec: %ld\n", diff2.tv_sec, diff2.tv_usec);
+    printf("scalar: %ld.%09ld seconds\n", seconds1, nanoseconds1);
+    printf("SSE   : %ld.%09ld seconds\n", seconds2, nanoseconds2);
 }
