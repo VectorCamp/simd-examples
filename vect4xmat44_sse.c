d34efeb166c95270fa8b5d9871c2de1e777f8b46
gtsoul-tech <gtsoulkanakis@gmail.com>
2024-03-19 12:45:41 +0200

    rand_num, float compare, n loops, nanosecond calculate

--- vect4xmat44_sse.c
+++ vect4xmat44_sse.c
@@ -1,86 +1,84 @@
 #include <immintrin.h>
+#include <math.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#include <sys/time.h>
-
-#define N 4
+#include <time.h>
+#define N 100000
 
 int
 main() {
-
-    // allocate memory for array
+    float epsilon = 0.00001f;
     int are_equal = 1;
-    int count = 0;
-    float B[N][N] __attribute__((aligned(16)));
-    float vectorA[N] __attribute__((aligned(16)));
+    float B[4][4] __attribute__((aligned(16)));
+    float vectorA[4] __attribute__((aligned(16)));
     // result vector
-    float result[N] __attribute__((aligned(16)));
+    float result[4] __attribute__((aligned(16)));
     // implResult
-    float implResult[N] __attribute__((aligned(16)));
-    // fill vector with numbers
-    for (int i = 0; i < N; i++) {
-        vectorA[i] = (float) i;
+    float implResult[4] __attribute__((aligned(16)));
+    struct timespec start, mid, end;
+    // Use the current time in microseconds to seed the random number generator
+    srand(time(NULL));
+    // fill vector with random numbers
+    for (int i = 0; i < 4; i++) {
+        vectorA[i] = (float) rand() / RAND_MAX * 4;
     }
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            B[i][j] = (float) count++;
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            B[i][j] = (float) rand() / RAND_MAX * 4;
         }
     }
 
-    struct timeval tv1, tv2, tv3, diff1, diff2;
-    gettimeofday(&tv1, NULL);
+    clock_gettime(CLOCK_MONOTONIC, &start);
 
     // normal scalar version
-    for (int i = 0; i < N; i++) {
-        result[i] = 0.0f;
-        for (int j = 0; j < N; j++) {
-            result[i] += B[i][j] * vectorA[j];
+    for (int k = 0; k < N; k++) {
+        for (int i = 0; i < 4; i++) {
+            result[i] = 0.0f;
+            for (int j = 0; j < 4; j++) {
+                result[i] += B[i][j] * vectorA[j];
+            }
         }
     }
-    gettimeofday(&tv2, NULL);
+    clock_gettime(CLOCK_MONOTONIC, &mid);
 
     // SSE version
-    __m128 result_sse[N];
+    __m128 result_sse[4];
+    for (int k = 0; k < N; k++) {
+        for (int i = 0; i < 4; i++) {
+            result_sse[i] = _mm_setzero_ps();
+            for (int j = 0; j < 4; j += 4) {
+                __m128 B_vec = _mm_load_ps(&B[i][j]);
+                __m128 vectorA_vec = _mm_load_ps(&vectorA[j]);   // works because vectorA has 4 float elements
+                result_sse[i] = _mm_fmadd_ps(B_vec, vectorA_vec, result_sse[i]);
+            }
+        }
 
-    for (int i = 0; i < N; i++) {
-        result_sse[i] = _mm_setzero_ps();
-        for (int j = 0; j < N; j += 4) {
-            __m128 B_vec = _mm_load_ps(&B[i][j]);
-            __m128 vectorA_vec = _mm_load_ps(&vectorA[j]);   // works because vectorA has 4 float elements
-            result_sse[i] = _mm_fmadd_ps(B_vec, vectorA_vec, result_sse[i]);
+        // Convert back to regular float array
+        for (int i = 0; i < 4; i++) {
+            __m128 tmp = _mm_add_ps(result_sse[i], _mm_movehl_ps(result_sse[i], result_sse[i]));
+            tmp = _mm_add_ss(tmp, _mm_shuffle_ps(tmp, tmp, 1));
+            // Take first 32-bit float element
+            implResult[i] = _mm_cvtss_f32(tmp);
         }
     }
+    clock_gettime(CLOCK_MONOTONIC, &end);
 
-    // Convert back to regular float array
-    for (int i = 0; i < N; i++) {
-        __m128 tmp = _mm_add_ps(result_sse[i], _mm_movehl_ps(result_sse[i], result_sse[i]));
-        tmp = _mm_add_ss(tmp, _mm_shuffle_ps(tmp, tmp, 1));
-        // Take first 32-bit float element
-        implResult[i] = _mm_cvtss_f32(tmp);
+    long seconds1 = mid.tv_sec - start.tv_sec;
+    long nanoseconds1 = mid.tv_nsec - start.tv_nsec;
+    if (nanoseconds1 < 0) {
+        seconds1--;
+        nanoseconds1 += 1000000000;
     }
-    gettimeofday(&tv3, NULL);
-
-    // Get time difference
-    // https://stackoverflow.com/questions/1444428/time-stamp-in-the-c-programming-language
-    diff1.tv_sec = tv2.tv_sec - tv1.tv_sec;
-    diff1.tv_usec = tv2.tv_usec + (1000000 - tv1.tv_usec);
-    diff2.tv_sec = tv3.tv_sec - tv2.tv_sec;
-    diff2.tv_usec = tv3.tv_usec + (1000000 - tv2.tv_usec);
-
-    while (diff1.tv_usec > 1000000) {
-        diff1.tv_sec++;
-        diff1.tv_usec -= 1000000;
-        printf("updated diff1 tv_sec:%ld tv_usec:%ld\n", diff1.tv_sec, diff1.tv_usec);
+    long seconds2 = end.tv_sec - mid.tv_sec;
+    long nanoseconds2 = end.tv_nsec - mid.tv_nsec;
+    if (nanoseconds2 < 0) {
+        seconds2--;
+        nanoseconds2 += 1000000000;
     }
-    while (diff2.tv_usec > 1000000) {
-        diff2.tv_sec++;
-        diff2.tv_usec -= 1000000;
-        printf("updated diff2 tv_sec:%ld tv_usec:%ld\n", diff2.tv_sec, diff2.tv_usec);
-    }
-
     for (int i = 0; i < 4; i++) {
-        if (result[i] != implResult[i]) {
+        if (fabs(result[i] - implResult[i]) > epsilon) {
             are_equal = 0;
             break;
         }
@@ -90,6 +88,6 @@ main() {
     } else {
         printf("Vector result and implResult are not the same\n");
     }
-    printf("scalar: %ld sec, usec: %ld\n", diff1.tv_sec, diff1.tv_usec);
-    printf("SSE   : %ld sec, usec: %ld\n", diff2.tv_sec, diff2.tv_usec);
+    printf("scalar: %ld.%09ld seconds\n", seconds1, nanoseconds1);
+    printf("SSE   : %ld.%09ld seconds\n", seconds2, nanoseconds2);
 }
