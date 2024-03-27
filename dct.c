e8e76638719a481f7a5692bd20aabe84927c2dc7
gtsoul-tech <gtsoulkanakis@gmail.com>
2024-03-27 16:18:25 +0200

    phase1

--- dct.c
+++ dct.c
@@ -3,7 +3,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
+#define N 1000000000
 typedef uint16_t dctcoef;
+void
+print_vector(__m128i v) {
+    uint16_t buf[8];
+    _mm_storeu_si128((__m128i *) buf, v);
+    for (int i = 0; i < 8; i++) {
+        printf("%02x ", buf[i]);
+    }
+    printf("\n");
+}
+
 void
 dct4x4dc(dctcoef d[16]) {
     dctcoef tmp[16];
@@ -19,7 +31,9 @@ dct4x4dc(dctcoef d[16]) {
         tmp[2 * 4 + i] = d01 - d23;
         tmp[3 * 4 + i] = d01 + d23;
     }
-
+    for (int i = 0; i < 16; i = i + 4) {
+        printf("in dct_c %02x %02x %02x %02x\n", tmp[0 + i], tmp[1 + i], tmp[2 + i], tmp[3 + i]);
+    }
     for (int i = 0; i < 4; i++) {
         int s01 = tmp[i * 4 + 0] + tmp[i * 4 + 1];
         int d01 = tmp[i * 4 + 0] - tmp[i * 4 + 1];
@@ -35,37 +49,59 @@ dct4x4dc(dctcoef d[16]) {
 
 void
 dct4x4dc_sse(dctcoef d[16]) {
-    __m128i tmp[2];
-    tmp[0] = _mm_setzero_si128();   // Set first vector to zero
-    tmp[1] = _mm_setzero_si128();   // Set second vector to zero
+    dctcoef dT[4][4];   // This will hold the transposed matrix
 
-    for (int i = 0; i < 2; i++) {
-        __m128i row1 = _mm_loadu_si128((__m128i *) &d[i * 8]);
-        __m128i row2 = _mm_loadu_si128((__m128i *) &d[i * 8 + 4]);
+    // Load the rows of d into 128-bit vectors
+    __m128i row1row2 = _mm_set_epi16(d[7], d[6], d[5], d[4], d[3], d[2], d[1], d[0]);
+    __m128i row3row4 = _mm_set_epi16(d[15], d[14], d[13], d[12], d[11], d[10], d[9], d[8]);
 
-        __m128i s01s23 = _mm_add_epi16(row1, row2);
-        __m128i d01d23 = _mm_sub_epi16(row1, row2);
+    __m128i tmp1 = _mm_unpacklo_epi16(row1row2, row3row4);
+    __m128i tmp3 = _mm_unpackhi_epi16(row1row2, row3row4);
+    __m128i tmp1_new = _mm_unpacklo_epi16(tmp1, tmp3);
+    __m128i tmp3_new = _mm_unpackhi_epi16(tmp1, tmp3);
+    __m128i upper_half_tmp1 = _mm_srli_si128(tmp1_new, 8);
+    _mm_storel_epi64((__m128i *) &dT[1][0], upper_half_tmp1);
+    _mm_storel_epi64((__m128i *) &dT[0][0], _mm_move_epi64(tmp1_new));
+    _mm_storel_epi64((__m128i *) &dT[3][0], _mm_srli_si128(tmp3_new, 8));
+    _mm_storel_epi64((__m128i *) &dT[2][0], _mm_move_epi64(tmp3_new));
+    // load rows from transposed d
+    row1row2 = _mm_loadu_si128((__m128i *) dT[0]);
+    row3row4 = _mm_loadu_si128((__m128i *) dT[2]);
+    // 1st + 2nd +3rd +4rth
+    __m128i totalSum = _mm_add_epi16(row1row2, row3row4);
+    __m128i shuffled = _mm_shuffle_epi32(totalSum, _MM_SHUFFLE(2, 3, 3, 2));
+    totalSum = _mm_add_epi16(totalSum, shuffled);
+    _mm_storel_epi64((__m128i *) dT[0], totalSum);
+    // 1st + 2nd -3rd -4rth
+    totalSum = _mm_sub_epi16(row1row2, row3row4);
+    shuffled = _mm_shuffle_epi32(totalSum, _MM_SHUFFLE(2, 3, 3, 2));
+    totalSum = _mm_add_epi16(totalSum, shuffled);
+    _mm_storel_epi64((__m128i *) dT[1], totalSum);
+    // 1st - 2nd -3rd +4rth
+    __m128i mask1 = _mm_setr_epi16(0, 0, 0, 0, -1, -1, -1, -1);
+    __m128i mask2 = _mm_setr_epi16(-1, -1, -1, -1, 0, 0, 0, 0);
+    __m128i zero = _mm_setzero_si128();
+    __m128i signMask1 = _mm_cmplt_epi16(mask1, zero);
+    __m128i signMask2 = _mm_cmplt_epi16(mask2, zero);
+    row1row2 = _mm_sub_epi16(_mm_andnot_si128(signMask1, row1row2), _mm_and_si128(signMask1, row1row2));
+    row3row4 = _mm_sub_epi16(_mm_andnot_si128(signMask2, row3row4), _mm_and_si128(signMask2, row3row4));
+    totalSum = _mm_add_epi16(row1row2, row3row4);
+    shuffled = _mm_shuffle_epi32(totalSum, _MM_SHUFFLE(2, 3, 3, 2));
+    totalSum = _mm_add_epi16(totalSum, shuffled);
+    _mm_storel_epi64((__m128i *) dT[2], totalSum);
+    // 1st - 2nd +3rd -4rth
+    totalSum = _mm_sub_epi16(row1row2, row3row4);
+    shuffled = _mm_shuffle_epi32(totalSum, _MM_SHUFFLE(2, 3, 3, 2));
+    totalSum = _mm_add_epi16(totalSum, shuffled);
+    _mm_storel_epi64((__m128i *) dT[3], totalSum);
 
-        __m128i tmp1 = _mm_unpacklo_epi64(s01s23, d01d23);
-        __m128i tmp2 = _mm_unpackhi_epi64(s01s23, d01d23);
-
-        tmp[i] = _mm_unpacklo_epi64(tmp1, tmp2);
-    }
-    for (int i = 0; i < 2; i++) {
-        __m128i vec = tmp[i];
-
-        __m128i s01s23 = _mm_add_epi16(vec, _mm_shuffle_epi32(vec, _MM_SHUFFLE(1, 0, 3, 2)));
-        __m128i d01d23 = _mm_sub_epi16(vec, _mm_shuffle_epi32(vec, _MM_SHUFFLE(1, 0, 3, 2)));
-
-        __m128i res1 = _mm_add_epi16(s01s23, _mm_set1_epi16(1));
-        __m128i res2 = _mm_add_epi16(d01d23, _mm_set1_epi16(1));
-
-        res1 = _mm_srai_epi16(res1, 1);
-        res2 = _mm_srai_epi16(res2, 1);
-
-        _mm_storeu_si128((__m128i *) &d[i * 8], res1);
-        _mm_storeu_si128((__m128i *) &d[i * 8 + 4], res2);
-    }
+    /*
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            printf("%02x ", (uint16_t) dT[i][j]);
+        }
+        printf("\n");
+    }*/
 }
 int
 main() {
@@ -74,7 +110,7 @@ main() {
     dctcoef matrix[16];
     dctcoef matrix2[16];
     for (int i = 0; i < 16; i++) {
-        matrix[i] = rand() % 65536;
+        matrix[i] = rand() & 0xFF;   // 8 bit unsigned
         matrix2[i] = matrix[i];
     }
     printf("Original matrix:\n");
@@ -87,14 +123,14 @@ main() {
     dct4x4dc_sse(matrix2);
     clock_gettime(CLOCK_MONOTONIC, &end);
 
-    printf("\nMatrix after dct4x4dc:\n");
+    // printf("\nMatrix after dct4x4dc:\n");
     for (int i = 0; i < 16; i += 4) {
-        printf("%02x %02x %02x %02x\n", matrix[i], matrix[i + 1], matrix[i + 2], matrix[i + 3]);
+        // printf("%02x %02x %02x %02x\n", matrix[i], matrix[i + 1], matrix[i + 2], matrix[i + 3]);
     }
 
-    printf("\nMatrix2 after dct4x4dc_sse:\n");
+    // printf("\nMatrix2 after dct4x4dc_sse:\n");
     for (int i = 0; i < 16; i += 4) {
-        printf("%02x %02x %02x %02x\n", matrix2[i], matrix2[i + 1], matrix2[i + 2], matrix2[i + 3]);
+        // printf("%02x %02x %02x %02x\n", matrix2[i], matrix2[i + 1], matrix2[i + 2], matrix2[i + 3]);
     }
 
     long seconds1 = mid.tv_sec - start.tv_sec;
